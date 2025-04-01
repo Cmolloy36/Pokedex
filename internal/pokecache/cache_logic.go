6fb68baed87b27dca668f9a55d199270a5224f2b
Cmolloy36 <cdmolloy36@gmail.com>
2025-04-01 18:35:08 -0400

    finished cache

---
+++ internal/pokecache/cache_logic.go
@@ -0,0 +1,77 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	Cache    map[string]cacheEntry
+	mu       *sync.Mutex
+	interval time.Duration
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+type BatchInfo struct {
+	Count    int     `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+func NewCache(interval time.Duration) *Cache {
+
+	newCache := &Cache{
+		Cache:    make(map[string]cacheEntry),
+		mu:       &sync.Mutex{},
+		interval: interval,
+	}
+
+	go newCache.reapLoop()
+
+	return newCache
+}
+
+func (c Cache) Add(key string, val []byte) {
+	newCacheEntry := cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+	c.mu.Lock()
+	c.Cache[key] = newCacheEntry
+	c.mu.Unlock()
+}
+
+func (c Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	cacheEntry, ok := c.Cache[key]
+	c.mu.Unlock()
+	if !ok {
+		return []byte{}, false
+	}
+
+	return cacheEntry.val, true
+}
+
+func (c Cache) reapLoop() {
+	ticker := time.NewTicker(c.interval)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		c.mu.Lock()
+		for key, entry := range c.Cache {
+			if time.Since(entry.createdAt) >= c.interval {
+				// fmt.Printf("%s deleted\n", key)
+				delete(c.Cache, key)
+			}
+		}
+		c.mu.Unlock()
+	}
+}
